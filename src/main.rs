4a41790b14d5bdf76ddba922b876bdb64e47f692
oscar <90446228+Eldolfin@users.noreply.github.com>
2023-04-12 22:55:53 +0100

    cleanup code and fix encoding problem

--- src/main.rs
+++ src/main.rs
@@ -4,24 +4,24 @@ use std::{env::args, error::Error, fs::File, io::Read};
 const OUTPUT_FILENAME: &str = "output.csv";
 
 fn main() -> Result<(), Box<dyn Error>> {
-    // prends le deuxieme argument en tant que nom de fichier
     let filename = args().nth(1).expect("Usage: vcf_to_csv FILENAME");
 
     println!("Converting {filename} 🤓");
 
     let mut file = File::open(filename).expect("This file doesn't exist 😢");
 
-    // Le fichier n'est peut etre pas en utf-8 donc il faut le convertir
+    // The file might not be in utf-8 so we convert it
     let mut buf = vec![];
     file.read_to_end(&mut buf)?;
-    let contents = String::from_utf8_lossy(&buf);
+    // conversion happens here
+    let contents: String = buf.iter().map(|&c| c as char).collect();
 
     // Parse vcards
     let vcards = VcardParser::new(contents.as_bytes());
 
     let mut csv = csv::Writer::from_path(OUTPUT_FILENAME)?;
 
-    csv.write_record(&["email", "name", "attributes"])?;
+    csv.write_record(["email", "name", "attributes"])?;
 
     for vc in vcards {
         let mut vc = vc
@@ -34,7 +34,7 @@ fn main() -> Result<(), Box<dyn Error>> {
         let other_properties = vc
             .properties
             .iter()
-            .map(|p| {
+            .filter_map(|p| {
                 // maps a property to json format
                 Some(format!(
                     "\"{name}\":\"{value}\"",
@@ -42,29 +42,29 @@ fn main() -> Result<(), Box<dyn Error>> {
                     value = p.value.clone()?
                 ))
             })
-            // to remove nones (where there is no value)
-            .flatten()
             .collect::<Vec<_>>()
             .join(", ");
 
         // adds {} around json
         let other_properties = format!("{{{other_properties}}}");
 
-        let record = [email, name, other_properties];
-        csv.write_record(&record)?;
+        csv.write_record([email, name, other_properties])?;
     }
+
+    println!("Done converting to {OUTPUT_FILENAME} 😎");
 
     Ok(())
 }
 
 fn get_property_and_remove(vc: &mut VcardContact, property: &str) -> String {
     if let Some(index) = vc.properties.iter().position(|x| x.name == property) {
-        if let Some(text) = vc.properties.remove(index).value {
+        let prop = vc.properties.remove(index);
+        if let Some(text) = prop.value {
             text
         } else {
-            panic!("Could get the {property} data for {vc:?}");
+            panic!("Could get the {property} data for {prop} 😨");
         }
     } else {
-        panic!("Could not find a `{property}` property for {vc:?}");
+        panic!("Could not find a `{property}` property for {vc:?} 😨");
     }
 }
